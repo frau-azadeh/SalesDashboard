099d415a4e3dfb6917397d10654af22d521a6071
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-11 22:10:36 +0330

    Add project files.

---
+++ SalesDashboard/ClientApp/tailwind.config.js
@@ -0,0 +1,13 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "../Views/**/*.cshtml",
+        "../Pages/**/*.cshtml",      // (در صورت وجود Razor Pages)
+        "../Areas/**/*.cshtml",      // (در صورت وجود Area)
+        "../wwwroot/js/**/*.js"      // (در صورت استفاده از فایل‌های JS)
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
