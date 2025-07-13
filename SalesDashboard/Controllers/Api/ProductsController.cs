c8d77c6de758cc0054203376d0d858b55a2a30c6
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-13 18:09:58 +0330

    add file with data for local

---
+++ SalesDashboard/Controllers/Api/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿// Controllers/Api/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+using SalesDashboard.Models;
+
+namespace SalesDashboard.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new()
+        {
+            new Product { Id = 1, Name = "کیبورد", Price = 700_000, Stock = 10 },
+            new Product { Id = 2, Name = "موس", Price = 450_000, Stock = 25 }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            product.Id = products.Max(p => p.Id) + 1;
+            products.Add(product);
+            return Ok(product);
+        }
+    }
+}
