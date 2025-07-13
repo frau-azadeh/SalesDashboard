2af89c1ba7b81d17a4bdc0725e5060547b041aec
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-13 18:11:03 +0330

    update

---
+++ SalesDashboard/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿// Models/Product.cs
+namespace SalesDashboard.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }  // Auto-increment
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; } // تعداد موجودی
+    }
+}
