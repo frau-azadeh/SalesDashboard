efc1282798a94be1641d5371724b52158bc49102
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-14 07:08:43 +0330

    create class DbContext

---
+++ SalesDashboard/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesDashboard.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        // در آینده می‌تونی جدول‌های دیگه هم اینجا اضافه کنی
+    }
+}
