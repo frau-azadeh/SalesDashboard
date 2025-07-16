57c235837c7749ea294953678aa1d92673be4da8
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 13:36:59 +0330

    add Models/Invoice

---
+++ SalesDashboard/Models/Invoice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesDashboard.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public int CusomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public string CreatedBy { get; set; }
+        public ICollection<InvoiceItem> Items { get; set; }
+    }
+}
