dcb0ed3b08a8cf8bf5db824ed06776956aece8b4
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 13:00:35 +0330

    add part LoginStatus and update

---
+++ SalesDashboard/Views/Shared/_LoginStatus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalesDashboard.Views.Shared
+{
+    public class _LoginStatusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
