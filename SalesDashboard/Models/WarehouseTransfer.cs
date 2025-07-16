4fcd2e18aa88a964bd78b75ab504939bf5fca503
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 13:38:57 +0330

    add Models/WarehouseTransfer

---
+++ SalesDashboard/Models/WarehouseTransfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SalesDashboard.Models
+{
+    public class WarehouseTransfer
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public string PerformedBy { get; set; } 
+    }
+}
