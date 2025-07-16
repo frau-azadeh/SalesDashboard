c836159641e846dc92b56ede1a6baf600b3afef0
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 16:44:50 +0330

    update Models/Customer

--- SalesDashboard/Models/Customer.cs
+++ SalesDashboard/Models/Customer.cs
@@ -1,12 +1,22 @@
-﻿namespace SalesDashboard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesDashboard.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
         public string FullName { get; set; }
 
         public string Email { get; set; }
 
         public string Phone { get; set; }
+
+        public string Company { get; set; }
+
+        public string Note { get; set; }
+
+        public ICollection<Invoice> Invoices { get; set; }
     }
 }
