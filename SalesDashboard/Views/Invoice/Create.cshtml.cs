5aebcef04c3c740e9e825b1f8e2c5b5f1ab22099
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 16:11:44 +0330

    update Invoice/Create

---
+++ SalesDashboard/Views/Invoice/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalesDashboard.Views.Invoice
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
