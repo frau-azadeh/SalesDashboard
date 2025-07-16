36dd5d01c2b3603b2d2f6f0b2bdf3925edad2b17
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-16 16:44:14 +0330

    build and update database

---
+++ SalesDashboard/Migrations/20250716130853_Fix.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Customers");
+        }
+    }
+}
