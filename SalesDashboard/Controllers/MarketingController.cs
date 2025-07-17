fd2e95c375db07edbdefb26e30fd05296ad4eb0e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-17 20:09:08 +0330

    update all Controller

---
+++ SalesDashboard/Controllers/MarketingController.cs
@@ -0,0 +1,13 @@
+﻿// Controllers/MarketingController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesDashboard.Controllers
+{
+    public class MarketingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
